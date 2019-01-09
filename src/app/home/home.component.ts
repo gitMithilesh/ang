6258f582f0900674f981bed5330a48b2d2d2ea6f
Mithilesh <mithilesh3kumar@gmail.com>
2019-01-09 12:24:22 +0530

    INITIAL

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  h1Style: boolean = false;
+  users: Object;
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.getUsers().subscribe(data => {
+      this.users = data
+      console.log(this.users);
+    });
+  }
+  h1Click() {
+    this.h1Style = true;
+  }
+  
+  firstClick() {
+    this.data.firstClick();
+  }
+
+}
